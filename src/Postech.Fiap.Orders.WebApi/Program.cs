2ad1bd641a24bc797d83f3b22de223f61a963f9b
Alexandre Nascimento <alexandre.n@outlook.com>
2025-01-19 13:33:39 -0300

    Add core architecture for Postech.Fiap.Orders.WebApi
    Implemented foundational code for the WebApi project, including middleware, health checks, validation, dependency injection, and result/error pattern. This establishes the base structure for handling HTTP requests, validation logic, exception management, and logging within the application.

---
+++ src/Postech.Fiap.Orders.WebApi/Program.cs
@@ -0,0 +1,22 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = AppSettings.Configuration();
+builder.Services.AddWebApi(configuration);
+builder.Services.AddSerilogConfiguration(builder, configuration);
+
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseHealthChecksConfiguration();
+app.UseExceptionHandler();
+app.UseSerilogRequestLogging();
+app.UseMiddleware<RequestContextLoggingMiddleware>();
+app.MapCarter();
+app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program;
