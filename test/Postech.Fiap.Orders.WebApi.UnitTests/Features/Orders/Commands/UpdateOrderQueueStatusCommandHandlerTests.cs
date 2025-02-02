3f9c4422cb694fae5ef7bcc2a9ad061360eb79e6
Alexandre Nascimento <alexandre.n@outlook.com>
2025-02-02 13:50:41 -0300

    Fix typo in "WepApi" by renaming to "WebApi" across tests
    Renamed files, namespaces, and references in the test project to correct the typo in "WepApi" to "WebApi". This ensures consistency and accuracy throughout the test files and related configurations in the solution.

---
+++ test/Postech.Fiap.Orders.WebApi.UnitTests/Features/Orders/Commands/UpdateOrderQueueStatusCommandHandlerTests.cs
@@ -0,0 +1,61 @@
+using Postech.Fiap.Orders.WebApi.Common.ResultPattern;
+using Postech.Fiap.Orders.WebApi.Features.Orders.Commands;
+using Postech.Fiap.Orders.WebApi.Features.Orders.Contracts;
+using Postech.Fiap.Orders.WebApi.Features.Orders.Entities;
+using Postech.Fiap.Orders.WebApi.Features.Orders.Services;
+
+namespace Postech.Fiap.Orders.WebApi.UnitTests.Features.Orders.Commands;
+
+public class UpdateOrderQueueStatusCommandHandlerTests
+{
+    private readonly UpdateOrderQueueStatusCommand.Handler _handler;
+    private readonly IOrderQueueService _orderQueueService;
+
+    public UpdateOrderQueueStatusCommandHandlerTests()
+    {
+        _orderQueueService = Substitute.For<IOrderQueueService>();
+        _handler = new UpdateOrderQueueStatusCommand.Handler(_orderQueueService);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFailure_WhenOrderNotFound()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        _orderQueueService.UpdateOrderStatusAsync(orderId, OrderQueueStatus.Completed, Arg.Any<CancellationToken>())
+            .Returns(Result.Failure<EnqueueOrderResponse>(Error.Failure("", "Order not found")));
+
+        var command = new UpdateOrderQueueStatusCommand.Command { Id = orderId, Status = OrderQueueStatus.Completed };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Message.Should().Be("Order not found");
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnSuccess_WhenOrderStatusIsUpdated()
+    {
+        // Arrange
+        var orderId = Guid.NewGuid();
+        var response = new EnqueueOrderResponse
+        {
+            OrderId = orderId,
+            Status = OrderQueueStatus.Completed
+        };
+
+        _orderQueueService.UpdateOrderStatusAsync(orderId, OrderQueueStatus.Completed, Arg.Any<CancellationToken>())
+            .Returns(Result.Success(response));
+
+        var command = new UpdateOrderQueueStatusCommand.Command { Id = orderId, Status = OrderQueueStatus.Completed };
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(response);
+    }
+}
